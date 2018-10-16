ae1387e26479a195baa147624be8d5c1a0a73e51
mariamika <joolveg@gmail.com>
2018-10-16 14:53:28 +0300

    Делала по уроку миграцию.

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -7,18 +7,33 @@ class TaskController extends Controller
 {
     public function actionIndex(){
 
-        $model = new MyModel();
-        $model->country = 'Indonesia';
-        $model->token = '465';
-        var_dump($model->token,$model->country);
-        var_dump($model->validate());
-        var_dump($model->getErrors());
-        exit;
+        //$model = new MyModel();
+        //$model->country = 'Indonesia';
+        //$model->token = '465';
+        //var_dump($model->token,$model->country);
+        //var_dump($model->validate());
+        //var_dump($model->getErrors());
+        //exit;
 
         return $this->render('index', [
             'title' => 'Framework Yii',
             'content' => 'Приветствую!!! =)',
 
         ]);
+    }
+
+    public function actionTest(){
+//        \Yii::$app->db->createCommand("
+//            insert into test(title,content,created) values
+//            ('title1','content1',NOW()),
+//            ('title2','content2',NOW()),
+//            ('title3','content3',NOW())
+//        ")->execute();
+
+//        $res = \Yii::$app->db->createCommand("
+//            select * from test
+//        ")->queryAll();
+
+//        var_dump($res); exit;
     }
 }
---
+++ migrations/m181016_112747_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `users`.
+ */
+class m181016_112747_create_users_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('users', [
+            'id' => $this->primaryKey(),
+            'login' => $this->string(50)->notNull(),
+            'password' => $this->string(100)->notNull(),
+            'role' => $this->integer()->defaultValue(1)
+        ]);
+
+        $this->createIndex('ix_users_login','users','login',true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('users');
+    }
+}
