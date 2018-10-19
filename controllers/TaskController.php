e20882b18cc0170e47faf3afbb306cfe9f380cc8
mariamika <joolveg@gmail.com>
2018-10-19 17:37:26 +0300

    Подключение базы данных, создание в ней таблиц длял примеров на уроке. Использование ActiveRecords для Users и Role. Работа с данными средствами Yii (чтение, запись, удаление, создание). Связывание двух таблиц.

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -1,19 +1,21 @@
 <?php
 namespace app\controllers;
 use app\models\MyModel;
+use app\models\tables\Users;
 use yii\web\Controller;
 
 class TaskController extends Controller
 {
     public function actionIndex(){
 
-        //$model = new MyModel();
-        //$model->country = 'Indonesia';
-        //$model->token = '465';
-        //var_dump($model->token,$model->country);
-        //var_dump($model->validate());
-        //var_dump($model->getErrors());
-        //exit;
+    /* Своя валидация
+        $model = new MyModel();
+        $model->country = 'Indonesia';
+        $model->token = '465';
+        var_dump($model->token,$model->country);
+        var_dump($model->validate());
+        var_dump($model->getErrors());
+        exit;  */
 
         return $this->render('index', [
             'title' => 'Framework Yii',
@@ -23,17 +25,42 @@ class TaskController extends Controller
     }
 
     public function actionTest(){
-//        \Yii::$app->db->createCommand("
-//            insert into test(title,content,created) values
-//            ('title1','content1',NOW()),
-//            ('title2','content2',NOW()),
-//            ('title3','content3',NOW())
-//        ")->execute();
 
-//        $res = \Yii::$app->db->createCommand("
-//            select * from test
-//        ")->queryAll();
+        /*Добавление в таблицу test новых данных
+        \Yii::$app->db->createCommand("
+            insert into test(title,content,created) values
+            ('title1','content1',NOW()),
+            ('title2','content2',NOW()),
+            ('title3','content3',NOW())
+        ")->execute();
+        $res = \Yii::$app->db->createCommand("select * from test")->queryAll();
+        var_dump($res);  */
 
-//        var_dump($res); exit;
+        /* Запись
+        $user = new Users();
+        $user->login = 'Pupkin';
+        $user->password = md5('qwerty');
+        $user->role = 1;
+
+        $user->save(); */
+
+        /* Чтение
+
+        $user = Users::findOne(1);
+        $user->isNewRecord = true;
+        $user->id = null;
+        $user->login = 'a d m i n';
+        $user->save(); */
+
+
+        /* Удаление
+        $user = Users::findOne(3);
+        $user->delete(); */
+
+        /* Связывание таблиц
+        $user = Users::findOne(1);
+        echo '<pre>'; var_dump($user->role); */
+
+        exit;
     }
 }
